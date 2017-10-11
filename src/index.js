d4b3871d0e4accddd1bb038905e1111dfd10eb78
Sara Schultz <saschultz95@gmail.com>
2017-10-11 12:17:43 -0700

    create clock element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 var greeting = React.createElement('h1', {}, 'Salut le monde');
+var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+
 
 ReactDOM.render(
   greeting,
