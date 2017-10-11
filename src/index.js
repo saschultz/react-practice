f79c5bc181769fd3c25f1b2c1c966e71c3a8337d
Sara Schultz <saschultz95@gmail.com>
2017-10-11 12:27:55 -0700

    call function in setInveral

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-function liveTime() {  
+function liveTime() {
   var greeting = React.createElement('h1', {}, 'Salut le monde');
   var question = React.createElement('h3', {}, 'Quelle heure est-il?');
   var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
@@ -10,3 +10,5 @@ function liveTime() {
     document.getElementById('react-app-root')
   );
 }
+
+setInterval(liveTime, 1000);
