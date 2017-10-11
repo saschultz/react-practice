6b3dc0079daf1842782621158d73c2e5126ab008
Sara Schultz <saschultz95@gmail.com>
2017-10-11 12:19:03 -0700

    create app element

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var greeting = React.createElement('h1', {}, 'Salut le monde');
 var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
+var app = ('div', {}, greeting, clock);
 
 
 ReactDOM.render(
