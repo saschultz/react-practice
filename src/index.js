f2ef17a916c213b8eea1a675615dab67eccdf0b8
Sara Schultz <saschultz95@gmail.com>
2017-10-11 13:19:35 -0700

    create header fo r help queue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-function liveTime() {
-  var greeting = React.createElement('h1', {}, 'Salut le monde');
-  var question = React.createElement('h3', {}, 'Quelle heure est-il?');
-  var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
-  var app = React.createElement('div', {}, greeting, question, clock);
+var heading = React.createElement('h1', {}, 'Help Queue');
+var app = React.createElement('div', {}, heading);
 
 
-  ReactDOM.render(
-    app,
-    document.getElementById('react-app-root')
-  );
-}
-
-setInterval(liveTime, 1000);
+ReactDOM.render(
+  app,
+  document.getElementById('react-app-root')
+);
